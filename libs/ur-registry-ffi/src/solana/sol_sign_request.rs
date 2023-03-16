7b221d953bd116a79d36a1ba2dbd7f8885724d68
Renfeng Shi <renfeng@keyst.one>
2023-03-16 15:05:36 +0800

    fix xfp

--- libs/ur-registry-ffi/src/solana/sol_sign_request.rs
+++ libs/ur-registry-ffi/src/solana/sol_sign_request.rs
@@ -13,14 +13,22 @@ export! {
         request_id: &str,
         sign_data: &str,
         path: &str,
-        xfp: u32,
+        xfp: &str,
         address: &str,
         origin: &str,
         sign_type: u32
     ) -> String {
-        let derivation_path = match CryptoKeyPath::from_path(path.to_string(), Some(xfp.to_be_bytes())) {
+        let xfp_bytes = match hex::decode(xfp) {
             Ok(v) => v,
-            Err(_) => return json!({"error": "path or xfp is invalid"}).to_string(),
+            Err(_) => return json!({"error": "xfp is invalid"}).to_string(),
+        };
+        let xfp_slice: [u8; 4] = match xfp_bytes.as_slice().try_into() {
+            Ok(v) => v,
+            Err(_) => return json!({"error": "length of xfp must be exactly 8"}).to_string(),
+        };
+        let derivation_path = match CryptoKeyPath::from_path(path.to_string(), Some(xfp_slice)) {
+            Ok(v) => v,
+            Err(_) => return json!({"error": "path is invalid"}).to_string(),
         };
         let sign_type = match SignType::from_u32(sign_type) {
             Ok(v) => v,
@@ -65,12 +73,12 @@ mod tests {
         let request_id = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
         let sign_data = "01000103c8d842a2f17fd7aab608ce2ea535a6e958dffa20caf669b347b911c4171965530f957620b228bae2b94c82ddd4c093983a67365555b737ec7ddc1117e61c72e0000000000000000000000000000000000000000000000000000000000000000010295cc2f1f39f3604718496ea00676d6a72ec66ad09d926e3ece34f565f18d201020200010c0200000000e1f50500000000";
         let path = "m/44'/501'/0'/0'";
-        let xfp = u32::from_str_radix("12345678", 16).unwrap();
+        let xfp = "12121212";
         let address = "";
         let origin = "solflare";
         let sign_type = 1;
 
-        let expect_result = "{\"cbor\":\"a501d825509b1deb4d3b7d4bad9bdd2b0d7b3dcb6d02589601000103c8d842a2f17fd7aab608ce2ea535a6e958dffa20caf669b347b911c4171965530f957620b228bae2b94c82ddd4c093983a67365555b737ec7ddc1117e61c72e0000000000000000000000000000000000000000000000000000000000000000010295cc2f1f39f3604718496ea00676d6a72ec66ad09d926e3ece34f565f18d201020200010c0200000000e1f5050000000003d90130a20188182cf51901f5f500f500f5021a123456780568736f6c666c6172650601\",\"type\":\"sol-sign-request\"}";
+        let expect_result = "{\"cbor\":\"a501d825509b1deb4d3b7d4bad9bdd2b0d7b3dcb6d02589601000103c8d842a2f17fd7aab608ce2ea535a6e958dffa20caf669b347b911c4171965530f957620b228bae2b94c82ddd4c093983a67365555b737ec7ddc1117e61c72e0000000000000000000000000000000000000000000000000000000000000000010295cc2f1f39f3604718496ea00676d6a72ec66ad09d926e3ece34f565f18d201020200010c0200000000e1f5050000000003d90130a20188182cf51901f5f500f500f5021a121212120568736f6c666c6172650601\",\"type\":\"sol-sign-request\"}";
 
         assert_eq!(expect_result, generate_sol_sign_request(
             request_id, sign_data, path, xfp, address, origin, sign_type
@@ -82,12 +90,12 @@ mod tests {
         let request_id = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
         let sign_data = "01000103c8d842a2f17fd7aab608ce2ea535a6e958dffa20caf669b347b911c4171965530f957620b228bae2b94c82ddd4c093983a67365555b737ec7ddc1117e61c72e0000000000000000000000000000000000000000000000000000000000000000010295cc2f1f39f3604718496ea00676d6a72ec66ad09d926e3ece34f565f18d201020200010c0200000000e1f50500000000";
         let path = "";
-        let xfp = u32::from_str_radix("12345678", 16).unwrap();
+        let xfp = "1212120";
         let address = "";
         let origin = "solflare";
         let sign_type = 1;
 
-        let err_result_derivation_path = "{\"error\":\"path or xfp is invalid\"}";
+        let err_result_derivation_path = "{\"error\":\"xfp is invalid\"}";
 
         assert_eq!(err_result_derivation_path, generate_sol_sign_request(
             request_id, sign_data, path, xfp, address, origin, sign_type
@@ -99,7 +107,7 @@ mod tests {
         let request_id = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
         let sign_data = "0123h";
         let path = "m/44'/501'/0'/0'";
-        let xfp = u32::from_str_radix("12345678", 16).unwrap();
+        let xfp = "12121212";
         let address = "";
         let origin = "solflare";
         let sign_type = 1;
