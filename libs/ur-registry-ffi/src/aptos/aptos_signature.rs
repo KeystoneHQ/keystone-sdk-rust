83e3cbbda929a256d305a89c8bf075b8b50cc165
LiYanLance <liyan1924@gmail.com>
2023-04-14 11:47:26 +0800

    Add type check for ur ffi

--- libs/ur-registry-ffi/src/aptos/aptos_signature.rs
+++ libs/ur-registry-ffi/src/aptos/aptos_signature.rs
@@ -4,14 +4,20 @@ use serde_json::json;
 use ur_registry::aptos::aptos_signature::AptosSignature;
 use ur_registry::traits::From;
 use uuid::Uuid;
+use ur_registry::registry_types::APTOS_SIGNATURE;
 
 use crate::export;
 
 export! {
     @Java_com_keystone_sdk_KeystoneNativeSDK_parseAptosSignature
 	fn parse_aptos_signature(
+        ur_type: &str,
 		cbor_hex: &str
 	) -> String {
+        if APTOS_SIGNATURE.get_type() != ur_type {
+            return json!({"error": "type not match"}).to_string();
+        }
+
         let parse_signature = || -> Result<(String, String, String), Error> {
             let cbor = hex::decode(cbor_hex.to_string())?;
             let sig = AptosSignature::from_cbor(cbor)?;
@@ -43,7 +49,15 @@ mod tests {
         let signature_cbor = "a301d825509b1deb4d3b7d4bad9bdd2b0d7b3dcb6d02584047e7b510784406dfa14d9fd13c3834128b49c56ddfc28edb02c5047219779adeed12017e2f9f116e83762e86f805c7311ea88fb403ff21900e069142b1fb310e0358208e53e7b10656816de70824e3016fc1a277e77825e12825dc4f239f418ab2e04e";
         let expect_result = "{\"authentication_public_key\":\"8e53e7b10656816de70824e3016fc1a277e77825e12825dc4f239f418ab2e04e\",\"request_id\":\"9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d\",\"signature\":\"47e7b510784406dfa14d9fd13c3834128b49c56ddfc28edb02c5047219779adeed12017e2f9f116e83762e86f805c7311ea88fb403ff21900e069142b1fb310e\"}";
 
-        assert_eq!(expect_result, parse_aptos_signature(signature_cbor));
+        assert_eq!(expect_result, parse_aptos_signature("aptos-signature", signature_cbor));
+    }
+
+    #[test]
+    fn test_parse_aptos_signature_type_error() {
+        let signature_cbor = "a301d825509b1deb4d3b7d4bad9bdd2b0d7b3dcb6d025841d4f0a7bcd95bba1fbb1051885054730e3f47064288575aacc102fbbf6a9a14daa066991e360d3e3406c20c00a40973eff37c7d641e5b351ec4a99bfe86f335f71303686b657973746f6e65";
+        let expect_result = "{\"error\":\"type not match\"}";
+
+        assert_eq!(expect_result, parse_aptos_signature("eth-signature", signature_cbor));
     }
 
     #[test]
@@ -51,6 +65,6 @@ mod tests {
         let signature_cbor = "a201";
         let expect_result = "{\"error\":\"signature is invalid\"}";
 
-        assert_eq!(expect_result, parse_aptos_signature(signature_cbor));
+        assert_eq!(expect_result, parse_aptos_signature("aptos-signature", signature_cbor));
     }
 }
--- libs/ur-registry-ffi/src/bitcoin/crypto_psbt.rs
+++ libs/ur-registry-ffi/src/bitcoin/crypto_psbt.rs
@@ -3,6 +3,7 @@ use anyhow::format_err;
 use hex;
 use serde_json::json;
 use ur_registry::crypto_psbt::CryptoPSBT;
+use ur_registry::registry_types::CRYPTO_PSBT;
 use ur_registry::traits::From;
 use ur_registry::traits::To;
 
@@ -11,8 +12,13 @@ use crate::export;
 export! {
     @Java_com_keystone_sdk_KeystoneNativeSDK_parseCryptoPSBT
 	fn parse_crypto_psbt(
+        ur_type: &str,
 		cbor_hex: &str
 	) -> String {
+        if CRYPTO_PSBT.get_type() != ur_type {
+            return json!({"error": "type not match"}).to_string();
+        }
+
         let parse = || -> Result<String, Error> {
             let cbor = hex::decode(cbor_hex.to_string())?;
             let psbt = CryptoPSBT::from_cbor(cbor).map_err(|_| format_err!(""))?;
@@ -55,7 +61,15 @@ mod tests {
         let cbor_hex = "58A770736274FF01009A020000000258E87A21B56DAF0C23BE8E7070456C336F7CBAA5C8757924F545887BB2ABDD750000000000FFFFFFFF838D0427D0EC650A68AA46BB0B098AEA4422C071B2CA78352A077959D07CEA1D0100000000FFFFFFFF0270AAF00800000000160014D85C2B71D0060B09C9886AEB815E50991DDA124D00E1F5050000000016001400AEA9A2E5F0F876A588DF5546E8742D1D87008F000000000000000000";
         let expect_result = "{\"psbt\":\"70736274ff01009a020000000258e87a21b56daf0c23be8e7070456c336f7cbaa5c8757924f545887bb2abdd750000000000ffffffff838d0427d0ec650a68aa46bb0b098aea4422c071b2ca78352a077959d07cea1d0100000000ffffffff0270aaf00800000000160014d85c2b71d0060b09c9886aeb815e50991dda124d00e1f5050000000016001400aea9a2e5f0f876a588df5546e8742d1d87008f000000000000000000\"}";
 
-        assert_eq!(expect_result, parse_crypto_psbt(cbor_hex));
+        assert_eq!(expect_result, parse_crypto_psbt("crypto-psbt", cbor_hex));
+    }
+
+    #[test]
+    fn test_parse_crypto_psbt_type_error() {
+        let cbor_hex = "58A770736274FF01009A020000000258E87A21B56DAF0C23BE8E7070456C336F7CBAA5C8757924F545887BB2ABDD750000000000FFFFFFFF838D0427D0EC650A68AA46BB0B098AEA4422C071B2CA78352A077959D07CEA1D0100000000FFFFFFFF0270AAF00800000000160014D85C2B71D0060B09C9886AEB815E50991DDA124D00E1F5050000000016001400AEA9A2E5F0F876A588DF5546E8742D1D87008F000000000000000000";
+        let expect_result = "{\"error\":\"type not match\"}";
+
+        assert_eq!(expect_result, parse_crypto_psbt("bytes", cbor_hex));
     }
 
     #[test]
@@ -63,7 +77,7 @@ mod tests {
         let cbor_hex = "a201";
         let expect_result = "{\"error\":\"PSBT is invalid\"}";
 
-        assert_eq!(expect_result, parse_crypto_psbt(cbor_hex));
+        assert_eq!(expect_result, parse_crypto_psbt("crypto-psbt", cbor_hex));
     }
 
     #[test]
