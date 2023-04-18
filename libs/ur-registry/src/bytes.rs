3afd254d58cf28a12db4230eabeb5557939e1f3d
yu@keyst.one <yu@keyst.one>
2023-04-18 13:16:41 +0800

    feat: add ur bytes type

---
+++ libs/ur-registry/src/bytes.rs
@@ -0,0 +1,91 @@
+use alloc::string::ToString;
+use alloc::vec::Vec;
+use minicbor::encode::Write;
+use minicbor::{Decoder, Encoder};
+use crate::error::{URError, URResult};
+use crate::registry_types::RegistryType;
+use crate::registry_types::BYTES as BYTES_TYPE;
+
+use crate::traits::{RegistryItem, To, From as FromCbor};
+use crate::types::Bytes as bytes;
+
+#[derive(Debug, Clone, Default)]
+pub struct Bytes(bytes);
+
+
+impl Bytes {
+    pub fn new(bytes: bytes) -> Self {
+        Bytes(bytes)
+    }
+
+    pub fn get_bytes(&self) -> bytes {
+        self.0.clone()
+    }
+
+    pub fn set_bytes(&mut self, bytes: bytes) {
+        self.0 = bytes;
+    }
+}
+
+impl RegistryItem for Bytes {
+    fn get_registry_type() -> RegistryType<'static> {
+        BYTES_TYPE
+    }
+}
+
+impl<C> minicbor::Encode<C> for Bytes {
+    fn encode<W: Write>(&self,
+                        e: &mut Encoder<W>,
+                        _ctx: &mut C) -> Result<(), minicbor::encode::Error<W::Error>> {
+        e.bytes(&self.0)?;
+        Ok(())
+    }
+}
+
+
+impl<'b, C> minicbor::Decode<'b, C> for Bytes {
+    fn decode(d: &mut Decoder<'b>, _ctx: &mut C) -> Result<Self, minicbor::decode::Error> {
+        Ok(Self(d.bytes()?.to_vec()))
+    }
+}
+
+impl To for Bytes {
+    fn to_bytes(&self) -> URResult<Vec<u8>> {
+        minicbor::to_vec(self.clone()).map_err(|e| URError::CborEncodeError(e.to_string()))
+    }
+}
+
+impl FromCbor<Bytes> for Bytes {
+    fn from_cbor(bytes: Vec<u8>) -> URResult<Bytes> {
+        minicbor::decode(&bytes).map_err(|e| URError::CborDecodeError(e.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::vec::Vec;
+    use crate::traits::{From as FromCbor, RegistryItem, To};
+    use hex::FromHex;
+    use crate::bytes::Bytes;
+
+    #[test]
+    fn test_encode() {
+        let crypto = Bytes(
+            Vec::from_hex("8c05c4b4f3e88840a4f4b5f155cfd69473ea169f3d0431b7a6787a23777f08aa").unwrap()
+        );
+        assert_eq!(
+            "58208C05C4B4F3E88840A4F4B5F155CFD69473EA169F3D0431B7A6787A23777F08AA",
+            hex::encode(crypto.to_bytes().unwrap()).to_uppercase()
+        );
+        let ur = ur::encode(&*(crypto.to_bytes().unwrap()), Bytes::get_registry_type().get_type());
+        assert_eq!(ur, "ur:bytes/hdcxlkahssqzwfvslofzoxwkrewngotktbmwjkwdcmnefsaaehrlolkskncnktlbaypkvoonhknt");
+    }
+
+    #[test]
+    fn test_decode() {
+        let part = "ur:bytes/hdcxlkahssqzwfvslofzoxwkrewngotktbmwjkwdcmnefsaaehrlolkskncnktlbaypkvoonhknt";
+        let decode_data = ur::decode(&part);
+        let crypto = Bytes::from_cbor(decode_data.unwrap().1).unwrap();
+        assert_eq!(crypto.get_bytes(), Vec::from_hex("8c05c4b4f3e88840a4f4b5f155cfd69473ea169f3d0431b7a6787a23777f08aa").unwrap());
+    }
+}
--- libs/ur-registry/src/registry_types.rs
+++ libs/ur-registry/src/registry_types.rs
@@ -5,6 +5,7 @@ use crate::error::{URError, URResult};
 pub enum URType {
     CryptoPsbt(String),
     CryptoAccount(String),
+    Bytes(String)
 }
 
 impl URType {
@@ -15,6 +16,9 @@ impl URType {
             }
             "crypto-account" => {
                 Ok(URType::CryptoAccount(type_str.to_string()))
+            }
+            "bytes" => {
+                Ok(URType::Bytes(type_str.to_string()))
             }
             _ => {
                 Err(URError::NotSupportURTypeError(type_str.to_string()))
@@ -30,46 +34,50 @@ impl URType {
             URType::CryptoAccount(type_str) => {
                 type_str.to_string()
             }
+            URType::Bytes(type_str) => {
+                type_str.to_string()
+            }
         }
     }
 }
 
 
-pub struct RegistryType<'a>(&'a str, u64);
+pub struct RegistryType<'a>(&'a str, Option<u64>);
 
 impl<'a> RegistryType<'_> {
     pub fn get_type(&self) -> String {
         self.0.to_string()
     }
     pub fn get_tag(&self) -> u64 {
-        self.1
+        self.1.unwrap_or(u64::MAX)
     }
 }
 
-pub const UUID: RegistryType = RegistryType("uuid", 37);
-pub const CRYPTO_HDKEY: RegistryType = RegistryType("crypto-hdkey", 303);
-pub const CRYPTO_KEYPATH: RegistryType = RegistryType("crypto-keypath", 304);
-pub const CRYPTO_COIN_INFO: RegistryType = RegistryType("crypto-coin-info", 305);
-pub const CRYPTO_ECKEY: RegistryType = RegistryType("crypto-eckey", 306);
-pub const CRYPTO_OUTPUT: RegistryType = RegistryType("crypto-output", 308);
-pub const CRYPTO_PSBT: RegistryType = RegistryType("crypto-psbt", 310);
-pub const CRYPTO_ACCOUNT: RegistryType = RegistryType("crypto-account", 311);
+pub const BYTES: RegistryType = RegistryType("bytes", None);
+pub const UUID: RegistryType = RegistryType("uuid", Some(37));
+pub const CRYPTO_HDKEY: RegistryType = RegistryType("crypto-hdkey", Some(303));
+pub const CRYPTO_KEYPATH: RegistryType = RegistryType("crypto-keypath", Some(304));
+pub const CRYPTO_COIN_INFO: RegistryType = RegistryType("crypto-coin-info", Some(305));
+pub const CRYPTO_ECKEY: RegistryType = RegistryType("crypto-eckey", Some(306));
+pub const CRYPTO_OUTPUT: RegistryType = RegistryType("crypto-output", Some(308));
+pub const CRYPTO_PSBT: RegistryType = RegistryType("crypto-psbt", Some(310));
+pub const CRYPTO_ACCOUNT: RegistryType = RegistryType("crypto-account", Some(311));
 
 // Multiple Accounts
-pub const CRYPTO_MULTI_ACCOUNTS: RegistryType = RegistryType("crypto-multi-accounts", 1103);
+pub const CRYPTO_MULTI_ACCOUNTS: RegistryType = RegistryType("crypto-multi-accounts", Some(1103));
 
 // ETH
-pub const ETH_SIGN_REQUEST: RegistryType = RegistryType("eth-sign-request", 401);
-pub const ETH_SIGNATURE: RegistryType = RegistryType("eth-signature", 402);
+pub const ETH_SIGN_REQUEST: RegistryType = RegistryType("eth-sign-request", Some(401));
+pub const ETH_SIGNATURE: RegistryType = RegistryType("eth-signature", Some(402));
 // SOL
-pub const SOL_SIGN_REQUEST: RegistryType = RegistryType("sol-sign-request", 1101);
-pub const SOL_SIGNATURE: RegistryType = RegistryType("sol-signature", 1102);
+pub const SOL_SIGN_REQUEST: RegistryType = RegistryType("sol-sign-request", Some(1101));
+pub const SOL_SIGNATURE: RegistryType = RegistryType("sol-signature", Some(1102));
 // Cosmos
-pub const COSMOS_SIGN_REQUEST: RegistryType = RegistryType("cosmos-sign-request", 4101);
-pub const COSMOS_SIGNATURE: RegistryType = RegistryType("cosmos-signature", 4102);
+pub const COSMOS_SIGN_REQUEST: RegistryType = RegistryType("cosmos-sign-request", Some(4101));
+pub const COSMOS_SIGNATURE: RegistryType = RegistryType("cosmos-signature", Some(4102));
 // Tron
-pub const TRON_SIGN_REQUEST: RegistryType = RegistryType("tron-sign-request", 5101);
-pub const TRON_SIGNATURE: RegistryType = RegistryType("tron-signature", 5102);
+pub const TRON_SIGN_REQUEST: RegistryType = RegistryType("tron-sign-request", Some(5101));
+pub const TRON_SIGNATURE: RegistryType = RegistryType("tron-signature", Some(5102));
 // Aptos
-pub const APTOS_SIGN_REQUEST: RegistryType = RegistryType("aptos-sign-request", 3101);
-pub const APTOS_SIGNATURE: RegistryType = RegistryType("aptos-signature", 3102);
+pub const APTOS_SIGN_REQUEST: RegistryType = RegistryType("aptos-sign-request", Some(3101));
+pub const APTOS_SIGNATURE: RegistryType = RegistryType("aptos-signature", Some(3102));
