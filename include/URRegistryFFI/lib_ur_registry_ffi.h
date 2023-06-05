2e5afc439f4470531ebee4a2d545623c806d531b
LiYanLance <liyan1924@gmail.com>
2023-06-05 17:53:42 +0800

    Update account extra struct

--- include/URRegistryFFI/lib_ur_registry_ffi.h
+++ include/URRegistryFFI/lib_ur_registry_ffi.h
@@ -13,8 +13,8 @@ const char* parse_crypto_account(struct ExternError*, const char* ur_type, const
 const char* parse_crypto_multi_accounts(struct ExternError*, const char* ur_type, const char* cbor_hex);
 
 // Utils
-const char* get_uncompressed_key(struct ExternError*, const char* compressed_key)
-const char* derive_public_key(struct ExternError*, const char* xpub, const char* path)
+const char* get_uncompressed_key(struct ExternError*, const char* compressed_key);
+const char* derive_public_key(struct ExternError*, const char* xpub, const char* path);
 
 // BTC
 const char* generate_crypto_psbt(struct ExternError*, const char* psbt_hex);
