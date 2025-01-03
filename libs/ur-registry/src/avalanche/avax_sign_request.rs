b74e2142ccedfb9563b007a9802544c00165bccf
ww3512687 <15809188520@163.com>
2025-01-03 10:00:09 +0800

    update avax sign request ur-type

--- libs/ur-registry/src/avalanche/avax_sign_request.rs
+++ libs/ur-registry/src/avalanche/avax_sign_request.rs
@@ -2,7 +2,7 @@ use crate::cbor::{cbor_array, cbor_map};
 use crate::error::{URError, URResult};
 use crate::registry_types::{RegistryType, AVAX_SIGN_REQUEST, UUID};
 use crate::traits::{From as FromCbor, RegistryItem, To};
-use crate::types::Bytes;
+use crate::types::{Bytes, Fingerprint};
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 use minicbor::data::{Int, Tag};
@@ -11,6 +11,7 @@ use minicbor::{Decoder, Encoder};
 
 const REQUEST_ID: u8 = 1;
 const SIGN_DATA: u8 = 2;
+const MASTER_FINGERPRINT: u8 = 3;
 const XPUB: u8 = 6;
 const WALLET_INDEX: u8 = 7;
 
@@ -18,15 +19,23 @@ const WALLET_INDEX: u8 = 7;
 pub struct AvaxSignRequest {
     request_id: Bytes,
     sign_data: Bytes,
+    master_fingerprint: Fingerprint,
     xpub: String,
     wallet_index: u64,
 }
 
 impl AvaxSignRequest {
-    pub fn new(request_id: Bytes, sign_data: Bytes, xpub: String, wallet_index: u64) -> Self {
+    pub fn new(
+        request_id: Bytes,
+        sign_data: Bytes,
+        master_fingerprint: Fingerprint,
+        xpub: String,
+        wallet_index: u64,
+    ) -> Self {
         AvaxSignRequest {
             request_id,
             sign_data,
+            master_fingerprint,
             xpub,
             wallet_index,
         }
@@ -46,6 +55,10 @@ impl AvaxSignRequest {
 
     pub fn set_tx_data(&mut self, data: Bytes) {
         self.sign_data = data;
+    }
+
+    pub fn get_master_fingerprint(&self) -> Fingerprint {
+        self.master_fingerprint
     }
 
     pub fn get_xpub(&self) -> String {
@@ -77,11 +90,15 @@ impl<C> minicbor::Encode<C> for AvaxSignRequest {
         e: &mut Encoder<W>,
         _ctx: &mut C,
     ) -> Result<(), minicbor::encode::Error<W::Error>> {
-        e.map(4)?;
+        e.map(5)?;
         e.int(Int::from(REQUEST_ID))?
             .tag(Tag::Unassigned(UUID.get_tag()))?
             .bytes(&self.request_id)?;
         e.int(Int::from(SIGN_DATA))?.bytes(&self.sign_data)?;
+        e.int(Int::from(MASTER_FINGERPRINT))?.int(
+            Int::try_from(u32::from_be_bytes(self.master_fingerprint))
+                .map_err(|e| minicbor::encode::Error::message(e.to_string()))?,
+        )?;
         e.int(Int::from(XPUB))?.str(&self.xpub)?;
         e.int(Int::from(WALLET_INDEX))?.u64(self.wallet_index)?;
         Ok(())
@@ -102,6 +119,11 @@ impl<'b, C> minicbor::Decode<'b, C> for AvaxSignRequest {
                 }
                 SIGN_DATA => {
                     obj.sign_data = d.bytes()?.to_vec();
+                }
+                MASTER_FINGERPRINT => {
+                    let mfp = u32::try_from(d.int()?)
+                        .map_err(|e| minicbor::decode::Error::message(e.to_string()));
+                    obj.master_fingerprint = u32::to_be_bytes(mfp?);
                 }
                 XPUB => {
                     obj.xpub = d.str()?.to_string();
@@ -144,23 +166,25 @@ mod tests {
             request_id: [12, 34, 56, 78].to_vec(),
             sign_data: Vec::from_hex("00000000000000000001ed5f38341e436e5d46e2bb00b45d62ae97d1b050c64bc634ae10626739e35c4b0000000121e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff00000007000000000089544000000000000000000000000100000001512e7191685398f00663e12197a3d8f6012d9ea300000001db720ad6707915cc4751fb7e5491a3af74e127a1d81817abe9438590c0833fe10000000021e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff000000050000000000989680000000010000000000000000")
                 .unwrap(),
+            master_fingerprint: [01, 02, 03, 04],
             xpub: "xpub6DXryz8Kd7XchtXvDnkjara83shGJH8ubu7KZhHhPfp4L1shvDEYiFZm32EKHnyo4bva4gxXjabFGqY7fNs8Ggd4khYz2oNs2KYLf56a9GX".to_string(),
             wallet_index: 6,
             };
         let result: Vec<u8> = unsigned_data.try_into().unwrap();
+        println!("result = {:?}", hex::encode(&result));
         let ur = ur::encode(&result, AvaxSignRequest::get_registry_type().get_type());
-        assert_eq!(ur, "ur:avax-sign-request/hkaddmaeaeaeaeaecpaeaeaeahaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaofsndtnrtwecakobwdytkisbazcwmcyfwbznnqdlttbtdmdbnmtyltdmyhsrkvopkaeaeaeataeaeaeaeahykvyaeaeaeaeaeaeaeaeaeaeaeaeadaeaeaeadltjsmobwadtlrszmykmotnvsiymdolbzryqzoxfpfsndtnrtwecakobwdytkisbazcwmcyfwbznnqdlttbtdmdbnmtyltdmyhsrkvopkaeaeaeataeaeaeaechstjstdaeaeaeaeaeaeaeaeaeaeaeadaeaeaeadasinwdidvorkdyvajnlfvsdlvoioweynltckonylaeaeaeadhgtlvofmdmctfgbdhslurdcwgomefhwftoqdbzwtttpsssctvafzmnuogtwlzssnaeaeaeaefsndtnrtwecakobwdytkisbazcwmcyfwbznnqdlttbtdmdbnmtyltdmyhsrkvopkaeaeaeahaeaeaeaecaryiobtaeaeaeadaeaeaeaeaeaeaeaeecfhesrp");
+        assert_eq!(ur, "ur:avax-sign-request/onadtpdafybncpetglaohdueaeaeaeaeaeaeaeaeaeadweheeteeckfxjthlfgvorkaeqzhlidplmsttpfgdswgrsweeplbeidioesvlhhgraeaeaeadclvajkchsbssrndrwmaeiokntbfgdikspdykcpjyrhtbahurdameprdydipdkizmaeaeaeataeaeaeaeaeldghfzaeaeaeaeaeaeaeaeaeaeaeadaeaeaeadgydmjsmeisgumkwtamiavyclmsottpynaddpnnotaeaeaeaduyjpbktbjokkbzsfflgyzokbghmeotpejyvydioytpcschpywlfxlpmhrtlsfhvyaeaeaeaeclvajkchsbssrndrwmaeiokntbfgdikspdykcpjyrhtbahurdameprdydipdkizmaeaeaeahaeaeaeaeaemkmtlaaeaeaeadaeaeaeaeaeaeaeaeaxcyadaoaxaaamksjlksjokpidenfyhdjpkkknetgrieemhdiaisjyhdkofyjtjeimhsjphseteojkisflgefdetkpidkpemgrhtisfdisgdiyjoeegsehjkiskofyfehkinfghtjneoeyfegrfdjtkkjleeidkohseeiokshdimhsidfgfljshkemiygljketflioieeejeishkkneyjlgljkeygrhkgsiyecenhsesflhdatameoenglsw");
     }
 
     #[test]
     fn test_avax_decode() {
         let bytes =
-            Vec::from_hex("a401d82544123456780258de00000000000000000001ed5f38341e436e5d46e2bb00b45d62ae97d1b050c64bc634ae10626739e35c4b0000000121e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff00000007000000000089544000000000000000000000000100000001512e7191685398f00663e12197a3d8f6012d9ea300000001db720ad6707915cc4751fb7e5491a3af74e127a1d81817abe9438590c0833fe10000000021e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff00000005000000000098968000000001000000000000000006786f7870756236445872797a384b6437586368745876446e6b6a61726138337368474a4838756275374b5a684868506670344c3173687644455969465a6d3332454b486e796f34627661346778586a61624647715937664e7338476764346b68597a326f4e73324b594c663536613947580706")
+            Vec::from_hex("a501d825440c22384e0258de00000000000000000001ed5f38341e436e5d46e2bb00b45d62ae97d1b050c64bc634ae10626739e35c4b0000000121e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff00000007000000000089544000000000000000000000000100000001512e7191685398f00663e12197a3d8f6012d9ea300000001db720ad6707915cc4751fb7e5491a3af74e127a1d81817abe9438590c0833fe10000000021e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff000000050000000000989680000000010000000000000000031a0102030406786f7870756236445872797a384b6437586368745876446e6b6a61726138337368474a4838756275374b5a684868506670344c3173687644455969465a6d3332454b486e796f34627661346778586a61624647715937664e7338476764346b68597a326f4e73324b594c663536613947580706")
                 .unwrap();
         let data = AvaxSignRequest::try_from(bytes).unwrap();
         assert_eq!(
             data.get_tx_data(),
-            Vec::from_hex("000000000022000000050000000000000000000000000000000000000000000000000000000000000000000000023d9bdac0ed1d761330cf680efdeb1a42159eb387d6d2950c96f7d28f61bbe2aa000000070000000005f5e100000000000000000000000001000000018771921301d5bffff592dae86695a615bdb4a4413d9bdac0ed1d761330cf680efdeb1a42159eb387d6d2950c96f7d28f61bbe2aa000000070000000017c771d2000000000000000000000001000000010969ea62e2bb30e66d82e82fe267edf6871ea5f70000000157d5e23e2e1f460b618bba1b55913ff3ceb315f0d1acc41fe6408edc4de9facd000000003d9bdac0ed1d761330cf680efdeb1a42159eb387d6d2950c96f7d28f61bbe2aa00000005000000001dbd670d000000010000000000000000")
+            Vec::from_hex("00000000000000000001ed5f38341e436e5d46e2bb00b45d62ae97d1b050c64bc634ae10626739e35c4b0000000121e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff00000007000000000089544000000000000000000000000100000001512e7191685398f00663e12197a3d8f6012d9ea300000001db720ad6707915cc4751fb7e5491a3af74e127a1d81817abe9438590c0833fe10000000021e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff000000050000000000989680000000010000000000000000")
                 .unwrap()
         );
     }
