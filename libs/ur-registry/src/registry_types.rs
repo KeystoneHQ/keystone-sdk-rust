27f4f1c83b97c119d0d2dbaa1c0635c44154daf8
ww3512687 <15809188520@163.com>
2025-11-19 10:03:58 +0800

    update xrp batch ur types

--- libs/ur-registry/src/registry_types.rs
+++ libs/ur-registry/src/registry_types.rs
@@ -243,8 +243,8 @@ pub const IOTA_SIGNATURE: RegistryType = RegistryType("iota-signature", Some(850
 pub const IOTA_SIGN_HASH_REQUEST: RegistryType = RegistryType("iota-sign-hash-request", Some(8503));
 
 // XRP
-pub const XRP_BATCH_SIGN_REQUEST: RegistryType = RegistryType("xrp-batch-sign-request", Some(40001));
-pub const XRP_BATCH_SIGNATURE: RegistryType = RegistryType("xrp-batch-signature", Some(40002));
+pub const XRP_BATCH_SIGN_REQUEST: RegistryType = RegistryType("xrp-batch-sign-request", Some(48601));
+pub const XRP_BATCH_SIGNATURE: RegistryType = RegistryType("xrp-batch-signature", Some(48602));
 
 // Zcash
 pub const ZCASH_ACCOUNTS: RegistryType = RegistryType("zcash-accounts", Some(49201));
